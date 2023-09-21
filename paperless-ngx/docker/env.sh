7cf111cef38ed805e1f0538a8fc7265ea6f2dbab
Benoit Anastay <45088785+BenoitAnastay@users.noreply.github.com>
2023-09-21 23:47:40 +0200

    disable SC2086

--- paperless-ngx/docker/env.sh
+++ paperless-ngx/docker/env.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2086
 
 # Replace env 
 
 set -eu
 
-PAPERLESS_URL="$(jq --raw-output ".url // empty" $CONFIG_PATH)"
-PAPERLESS_FILENAME_FORMAT="$(jq --raw-output ".filename.format" $CONFIG_PATH)"
-PAPERLESS_OCR_LANGUAGE="$(jq --raw-output ".ocr.language" $CONFIG_PATH)"
-DEFAULT_USERNAME="$(jq --raw-output ".default_superuser.username" $CONFIG_PATH)"
-DEFAULT_EMAIL="$(jq --raw-output ".default_superuser.email" $CONFIG_PATH)"
-DEFAULT_PASSWORD="$(jq --raw-output ".default_superuser.password" $CONFIG_PATH)"
-PAPERLESS_TIME_ZONE="$(jq --raw-output ".timezone.timezone" $CONFIG_PATH)"
+PAPERLESS_URL=$(jq --raw-output ".url // empty" $CONFIG_PATH)
+PAPERLESS_FILENAME_FORMAT=$(jq --raw-output ".filename.format" $CONFIG_PATH)
+PAPERLESS_OCR_LANGUAGE=$(jq --raw-output ".ocr.language" $CONFIG_PATH)
+DEFAULT_USERNAME=$(jq --raw-output ".default_superuser.username" $CONFIG_PATH)
+DEFAULT_EMAIL=$(jq --raw-output ".default_superuser.email" $CONFIG_PATH)
+DEFAULT_PASSWORD=$(jq --raw-output ".default_superuser.password" $CONFIG_PATH)
+PAPERLESS_TIME_ZONE=$(jq --raw-output ".timezone.timezone" $CONFIG_PATH)
 PAPERLESS_CONSUMPTION_DIR=/share/paperless/consume
 PAPERLESS_DATA_DIR=/share/paperless/data
 PAPERLESS_MEDIA_ROOT=/share/paperless/media
